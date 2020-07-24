54156bcfd2ea39cf628dee40aee91ded7142039e
manuelbb <manuelbb@mail.uni-paderborn.de>
2020-07-24 10:12:59 +0200

    stdout in patch

--- patch_openssl_jll.jl
+++ patch_openssl_jll.jl
@@ -1,18 +1,20 @@
+current_out = stdout;
+redirect_stdout( open("/dev/null", "w" ) );
 using Pkg;
 using Pkg.Artifacts;
 
 # make a temporary environment
-envpath = tempname()
-mkdir(envpath)
+envpath = tempname();
+mkdir(envpath);
 
-Pkg.activate( envpath )
+Pkg.activate( envpath );
 
-Pkg.add( "OpenSSL_jll" )
+Pkg.add( "OpenSSL_jll" );
 
-hash_val = artifact_hash("OpenSSL", joinpath( dirname( Base.find_package("OpenSSL_jll") ), "..", "Artifacts.toml" ) )
-lib_path = joinpath( artifact_path(hash_val), "lib" )
+hash_val = artifact_hash("OpenSSL", joinpath( dirname( Base.find_package("OpenSSL_jll") ), "..", "Artifacts.toml" ) );
+lib_path = joinpath( artifact_path(hash_val), "lib" );
 
-Pkg.activate()
-rm( envpath, recursive = true, force = true)
-
-println(lib_path)
+Pkg.activate();
+rm( envpath, recursive = true, force = true);
+redirect_stdout(current_out);
+print(lib_path)
